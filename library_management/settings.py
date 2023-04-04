bb733d6fa4086d5b82c92c74b7ac60d713f4041c
Lob0tomy <szymoncz1331@gmail.com>
2023-04-04 13:00:28 +0300

    Small adjustments

--- library_management/settings.py
+++ library_management/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
@@ -39,8 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'books',
-    'users',
+    'apps.books',
+    'apps.users',
+    'phonenumber_field',
 ]
 
 MIDDLEWARE = [
