a72a067d1242e7568e426650bb728efe58a3a4a5
Lob0tomy <szymoncz1331@gmail.com>
2023-04-05 16:22:59 +0300

    Added UserSerializer

--- Backend/apps/users/serializers.py
+++ Backend/apps/users/serializers.py
@@ -1,1 +1,11 @@
-# TODO: Stworzyć serializery dla "user"
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'first_name', 'last_name', 'phone_no', 'created', 'borrowed_books', 'photo']
+
+
