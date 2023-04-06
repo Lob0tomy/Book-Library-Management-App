59a044ac42d15bdc8f6eecf25f9d6bbe5f32a395
Lob0tomy <szymoncz1331@gmail.com>
2023-04-06 16:40:14 +0300

    Added BookViewPermission

--- Backend/apps/books/views.py
+++ Backend/apps/books/views.py
@@ -1,7 +1,31 @@
-from rest_framework import generics, filters
+from rest_framework import generics, filters, status
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
 from django_filters.rest_framework import DjangoFilterBackend
 from apps.books.models import Book
 from apps.books.serializers import BookSerializer
+from .permissions import BookViewPermission
+
+
+# class BooksViewSet(ViewSet):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+#     permission_classes = [BookViewPermission]
+#
+#     def list(self, request):
+#         filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+#         filterset_fields = ['genre', 'author', 'publisher']
+#         search_fields = ['title', 'author', 'publisher']
+#         ordering_fields = ['pub_date', 'created', 'title', 'author']
+#         serializer = self.get_serializer(self.get_queryset(), many=True)
+#         return self.get_paginated_response(self.paginate_queryset(serializer.data))
+#
+#     def create(self, request, *args, **kwargs):
+#         serializer = self.get_serializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save(serializer)
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 
 class BookList(generics.ListCreateAPIView):
@@ -10,6 +34,7 @@ class BookList(generics.ListCreateAPIView):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [BookViewPermission]
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ['genre', 'author', 'publisher']
     search_fields = ['title', 'author', 'publisher']
@@ -22,5 +47,6 @@ class BookDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [BookViewPermission]
 
 
