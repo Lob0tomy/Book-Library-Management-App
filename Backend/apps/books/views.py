7bedbe8544a3ca906f410d31ffd00b979df80e90
Lob0tomy <szymoncz1331@gmail.com>
2023-04-06 20:57:35 +0300

    Added BookAdd view in "books"

--- Backend/apps/books/views.py
+++ Backend/apps/books/views.py
@@ -7,27 +7,6 @@ from apps.books.serializers import BookSerializer
 from .permissions import BookViewPermission
 
 
-# class BooksViewSet(ViewSet):
-#     queryset = Book.objects.all()
-#     serializer_class = BookSerializer
-#     permission_classes = [BookViewPermission]
-#
-#     def list(self, request):
-#         filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
-#         filterset_fields = ['genre', 'author', 'publisher']
-#         search_fields = ['title', 'author', 'publisher']
-#         ordering_fields = ['pub_date', 'created', 'title', 'author']
-#         serializer = self.get_serializer(self.get_queryset(), many=True)
-#         return self.get_paginated_response(self.paginate_queryset(serializer.data))
-#
-#     def create(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         serializer.save(serializer)
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-
-
 class BookList(generics.ListAPIView):
     """
     List all books stored in the library
